c01f498616b4c3a290f0a8b97dd4257640a07c87
sage417 <sage417@foxmail.com>
2018-11-08 23:35:27 +0800

    simple Add a Welcome Page

--- sso-simple/src/main/java/app/pooi/ssosimple/SsoSimpleApplication.java
+++ sso-simple/src/main/java/app/pooi/ssosimple/SsoSimpleApplication.java
@@ -3,12 +3,35 @@ package app.pooi.ssosimple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SsoSimpleApplication {
+@RestController
+public class SsoSimpleApplication extends WebSecurityConfigurerAdapter {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SsoSimpleApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
     }
 }
