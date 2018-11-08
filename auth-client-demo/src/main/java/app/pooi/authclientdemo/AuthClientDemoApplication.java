8f6c44d3bf5457fb318836d97fc5e9da5bd6e8ba
sage417 <sage417@foxmail.com>
2018-11-08 22:45:57 +0800

    init

---
+++ auth-client-demo/src/main/java/app/pooi/authclientdemo/AuthClientDemoApplication.java
@@ -0,0 +1,12 @@
+package app.pooi.authclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthClientDemoApplication.class, args);
+    }
+}
