9b94223002235b8c8552931b466413c3469d1f93
sage417 <sage417@foxmail.com>
2018-11-08 23:17:18 +0800

    simple step 1

---
+++ sso-simple/src/test/java/app/pooi/ssosimple/SsoSimpleApplicationTests.java
@@ -0,0 +1,16 @@
+package app.pooi.ssosimple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsoSimpleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
